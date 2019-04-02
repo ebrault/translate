6eb6b1428f4cf0a0e6d584ffd651ba65e5bc34db
ebrault <emmet.brault@gmail.com>
2019-04-02 12:07:05 -0400

    Built out language selector component
    Refactored boilerplate app component:
      - Changed returned div to multi-line JSX
      - Added a div to contain language selector icons and text
      - Added language selector icons
      - Initialized component based state for language with default language
        'english'
      - Added click handlers to language selector icons referencing callback
        function onChangeLanguage with the string of the language passed
      - Added onChangeLanguage above render method and below state init,
        ensured onChangeLanguage is an arrow function to avoid errors,
        accepts argument of language
      - Called this.setState in onChangeLanguage with language passed in as
        an argument
      - Rendered the language in the language selector div by calling
        this.state.language
      - Tested app to see that switches between language can occur

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,23 @@
 import React from 'react'
 
 class App extends React.Component {
+  state = { language: 'english' }
+
+  onChangeLanguage = language => {
+    this.setState({ language })
+  }
+
   render() {
-    return <div>App</div>;
+    return (
+      <div className="ui container">
+        <div>
+          Select A Language:
+          <i onClick={() => this.onChangeLanguage('english')} className="flag us" />
+          <i onClick={() => this.onChangeLanguage('dutch')} className="flag nl" />
+        </div>
+        {this.state.language}
+      </div>
+    );
   }
 }
 
