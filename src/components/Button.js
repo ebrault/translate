50634f475bc2836dea5870816570a10481fb43a2
ebrault <emmet.brault@gmail.com>
2019-04-02 12:36:52 -0400

    Created Contexts
    - Created contexts folder:
      - Added LanguageContext.js file to contexts folder
        - LanguageContext imports React and exports React.CreateContext() a
          function of React which creates the context object
          - CreateContext function can take in a default value, in this case
          the string 'english'
    - Wired up LanguageContext to Button component
      - Imported LanguageContext.js from contexts folder
      - Added static contextType variable to Button and set it to
        LanguageContext
      - Added a console log of this.context to Button component to show that
        the Button component is successfully receiving the context

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import LanguageContext from '../contexts/LanguageContext';
 
 class Button extends React.Component {
+
+  static contextType = LanguageContext;
+
   render() {
-    return (<button className="ui button primary">Submit</button>);
+    console.log(this.context);
+    return (
+      <button className="ui button primary">
+        Submit
+      </button>
+    );
   }
 }
 
